a16c72e3f719120853fbd6ed0ca390827063cc65
Enoch2k2 <enoch2k2@hotmail.com>
2018-08-02 13:00:48 -0500

    removed view test and updated welcome test

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe WelcomeController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
+    end
+
+    it "is the root of the website" do
+      visit root_path
+      expect(page).to have_current_path(root_path)
     end
   end
 
