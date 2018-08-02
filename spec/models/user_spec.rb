42f470dbe377edcf67e7ee64b4d23a2053411c89
Enoch2k2 <enoch2k2@hotmail.com>
2018-08-02 12:59:28 -0500

    added bcrypt and updated user tests for username and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  describe "attributes" do
+    it "has a username and password" do
+      user = User.create(username: "test", password: "test")
+
+      expect(user.username).to eq("test")
+      expect(user.password).to eq("test")
+    end
+  end
 end
