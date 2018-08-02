66f2d1abf787a9d3a1c3d7ed4724692badf93cbd
Enoch2k2 <enoch2k2@hotmail.com>
2018-08-02 13:46:55 -0500

    added poltergiest driver to testing and capybara

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ end
 group :test do
   gem 'capybara'
   gem 'database_cleaner'
+  gem 'poltergeist'
 end
 
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
