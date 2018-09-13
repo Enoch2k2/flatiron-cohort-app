8f787bdb38c33acda15385603c0428403894fe86
DakotaLMartinez <dakotaleemusic@gmail.com>
2018-09-12 19:25:46 -0700

    add redirect on not authorized error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   private
     def authorize_name
       redirect_to edit_user_path(current_user) if user_signed_in? && current_user.first_name.nil?
@@ -9,5 +11,10 @@ class ApplicationController < ActionController::Base
     
     def authorize_user
       redirect_to root_path if !user_signed_in? || !current_user.admin? || !current_user.instructor?
+    end
+
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to(request.referrer || root_path)
     end
 end
